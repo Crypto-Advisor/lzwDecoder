be68baa1812d8005b0c0523efb6f5b1f9b4e1e22
Kosi Nwabueze <kosinwabueze@gmail.com>
2020-09-09 11:11:24 -0700

    feature: implemented lzw compression BREAKING CHANGES!

---
+++ src/com/github/kosinw/lzw/LZWEncoder.java
@@ -0,0 +1,63 @@
+package com.github.kosinw.lzw;
+
+import java.io.*;
+import java.util.*;
+
+// 32-bit words
+
+public class LZWEncoder {
+    public static void encodeFile(String filename) {
+        try {
+            File file = new File(filename);
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+
+            File encoded = new File(file.getAbsolutePath() + ".lzw");
+            BufferedWriter writer = new BufferedWriter(new FileWriter(encoded));
+
+            int current = 0;
+
+            HashMap<String, Integer> dictionary = new HashMap<String, Integer>();
+
+            String P = "";
+
+            for (int i = 0; i < 128; ++i) {
+                dictionary.put("" + (char)i, i);
+            }
+
+            int currentKey = 128;
+
+            while ((current = reader.read()) != -1) {
+                //  Is the string P + C present in the dictionary?
+                // - If it is:    
+                //       - P = concat(P,C);
+                // - if Not:    
+                //        - output the code word which denotes P to the codestream
+                //        - add the string P+C) to the dictionary
+                //        - P = C 
+
+                String PC = P + (char)current;
+
+                if (dictionary.containsKey(PC)) {
+                    P = PC;
+                } else {
+                    writer.write(dictionary.get(P));
+                    dictionary.put(PC, currentKey);
+                    currentKey += 1;
+                    P = "" + (char)current;
+                }
+            }
+
+            reader.close();
+            writer.close();
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        String filename = args[0];
+
+        LZWEncoder.encodeFile(filename);
+    }
+}
