c01921c6541dca5ae4c85f35cce9837ee8d3a923
nghodsi <47699320+nghodsi@users.noreply.github.com>
2020-09-18 09:57:27 -0700

    Update LZWDecoder.java

--- LZWDecoder.java
+++ LZWDecoder.java
@@ -2,14 +2,14 @@ import java.util.*;
 import java.io.*;
 
 public class LZWDecoder{
-	public static void encodeFile (String filename){
-		try{
+	public static void decodeFile (String filename){ //File name of encoded file as parameter
+		try{ //Tests code for errors while it's being executed
 			File inputFile = new File (filename);
 			FileReader fReader = new FileReader (inputFile);
 			BufferedReader bReader = new BufferedReader (fReader);
 
-			File outputFile = new File (filename.substring(0,filename.length()-4));
-			FileWriter fWriter = new FileWriter (outputFile);
+			File outputFile = new File (filename.substring(0,filename.length()-4)); //Creates output file with encoded file name minus ".lzw"
+			FileWriter fWriter = new FileWriter (outputFile); //Sets up file writer to edit new file
 			BufferedWriter bWriter = new BufferedWriter (fWriter);
 			
 	        ArrayList<Integer> output = new ArrayList<Integer>();
@@ -20,11 +20,11 @@ public class LZWDecoder{
 	        	currentLine = currentLine.substring (currentLine.indexOf(" ")+1);
 	        }
 
-        	int dictionarySize = 256;
+        	int dictionarySize = 256; // Number of ASCII symbols 0-255
 
-       		HashMap<Integer,String> dictionary = new HashMap<Integer,String>();
+       		HashMap<Integer,String> dictionary = new HashMap<Integer,String>(); //Dictionary of ASCII symbols and any symbols added in the code
        		
-        	for (int i = 0; i < 256; i++){
+        	for (int i = 0; i < 256; i++){ //Adding ASCII symbols to dictionary hashap
             	dictionary.put(i, "" + (char)i);
         	}
  
@@ -49,15 +49,15 @@ public class LZWDecoder{
         	}
 
 			bWriter.write(resultString.toString());
-			bWriter.close();
+			bWriter.close(); //Close readers and writers to release system resources
 			bReader.close();    	
 
-	    }catch (Exception exe){
+	    }catch (Exception exe){ //Executes exception if an error is found in the try block
 	    	exe.printStackTrace();
 	    }
 	}
 	public static void main (String [] args){
-		String filename = "smol-movie.txt.lzw";
-		LZWDecoder.encodeFile(filename);
+		String filename = "smol-movie.txt.lzw"; //Sets filename variable
+		LZWDecoder.decodeFile(filename); //Runs decode function
 	}
 }
