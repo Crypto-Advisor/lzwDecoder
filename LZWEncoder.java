dd17a59dcb8ddb7eb9cbfe56b19602e52078b6a2
saculgrad <70218089+saculgrad@users.noreply.github.com>
2020-09-18 02:39:04 -0700

    Change to encoder code
    Outputted the additional dictionary entries before the list of codes. This makes it much easier to quickly add them to a dictionary in the decoder.

---
+++ LZWEncoder.java
@@ -0,0 +1,78 @@
+//package com.github.kosinw.lzw;
+
+import java.io.*;
+import java.util.*;
+
+// 32-bit words
+
+public class LZWEncoder {
+    public static void encodeFile(String filename) {
+        try {
+            File file = new File(filename);
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+
+            File encoded = new File(file.getAbsolutePath() + ".lzw");
+            BufferedWriter writer = new BufferedWriter(new FileWriter(encoded));
+
+            int current = 0;
+
+            HashMap<String, Integer> dictionary = new HashMap<String, Integer>();
+
+            String P = "";
+            String encodedString = "";
+
+            int currentKey = 0x100;
+
+            for (int i = 0; i < currentKey; ++i) {
+                dictionary.put("" + (char)i, i);
+            }
+
+            int max = 0x10000;
+
+            while ((current = reader.read()) != -1) {
+                //  Is the string P + C present in the dictionary?
+                // - If it is:    
+                //       - P = concat(P,C);
+                // - if Not:    
+                //        - output the code word which denotes P to the codestream
+                //        - add the string P+C) to the dictionary
+                //        - P = C
+
+                String PC = P + (char)current;
+                
+                if (dictionary.containsKey(PC)) {
+                    P = PC;
+                } else if (currentKey >= max) {
+                    encodedString += dictionary.get(P) + " ";
+                    P = "" + (char)current;
+                } else {
+                    encodedString += dictionary.get(P) + " ";
+                    dictionary.put(PC, currentKey);
+                    writer.write(PC +"\n");
+                    currentKey += 1;
+                    P = "" + (char)current;
+                }
+            }
+
+            if (!P.equals("")) {
+                if (dictionary.containsKey(P)) {
+                    encodedString += dictionary.get(P) + " ";
+                } else {
+                    encodedString += P;
+                }
+            }
+            writer.write(encodedString);
+            reader.close();
+            writer.close();
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        String filename = "lzw-file1.txt";
+        LZWEncoder.encodeFile(filename);
+        //Outputs a set of space delimited integers corresponding to each character's location within the library
+    }
+}
