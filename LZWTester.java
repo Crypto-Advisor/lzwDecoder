64b92a755b08ae8da0d66b44b3b4ff5b1a3712c9
Luke Riley <lukegough@me.com>
2020-09-18 10:47:52 -0700

    created tester

---
+++ LZWTester.java
@@ -0,0 +1,8 @@
+public class LZWTester {
+	public static void main(String[] args){
+		LZWEncoder encoder = new LZWEncoder();
+		encoder.encodeFile("lzw-file1.txt");
+		//LZWDecoder decoder = new LZWDecoder();
+		//decoder.decode("lz")
+	}
+}
